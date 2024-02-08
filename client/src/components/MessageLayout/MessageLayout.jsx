e36dc384bfb5526a04e476fd6c3ff8e256888ed9
Tmoh-Squim <122232330+Tmoh-Squim@users.noreply.github.com>
2024-02-08 10:21:07 +0300

    updated

--- client/src/components/MessageLayout/MessageLayout.jsx
+++ client/src/components/MessageLayout/MessageLayout.jsx
@@ -426,12 +426,12 @@ const Coversation = ({
                             {message?.deletedForAll ? (
                               <>
                                 {senderMessage ? (
-                                  <p className="bg-neutral-500 rounded-xl w-max p-1 h-min">
-                                    you deleted this message
+                                  <p className="bg-neutral-500 rounded-xl italic w-max p-1 h-min">
+                                  <span className="text-red-500 w-[15px] h-[15px] rounded-full text-center">x</span>   You deleted this message
                                   </p>
                                 ) : (
-                                  <p className="bg-neutral-500 rounded-xl w-max p-1 h-min">
-                                    this message wasdeleted
+                                  <p className="bg-neutral-500 rounded-xl italic w-max p-1 h-min">
+                                  <span className="text-red-500 w-[15px]  h-[15px] rounded-full text-center">x</span>  This message was deleted
                                   </p>
                                 )}
                               </>
