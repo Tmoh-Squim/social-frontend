4e4330d2ad30cfee1f89ec023e1b91dcf8d2cd2d
Tmoh-Squim <122232330+Tmoh-Squim@users.noreply.github.com>
2024-02-11 15:26:22 +0300

    updated

--- client/src/components/user/ChangePassword.jsx
+++ client/src/components/user/ChangePassword.jsx
@@ -2,13 +2,13 @@ import React,{useState} from "react";
 import axios from "axios"
 import {toast} from "react-toastify"
 import {ServerUrl} from "../../server.tsx"
+import {useParams} from "react-router-dom"
 import {useSelector} from "react-redux"
 const ChangePassword = () => {
     const [pass, setPass] = useState("");
     const [password, setPassword] = useState("");
     const [oldPass, setOldPass] = useState("");
-    const {user} = useSelector((state)=>state.user?.user)
-    const {id} = user?._id
+    const id = useParams()
     const handleSubmit = async()=>{
       try {
         if(pass !== password){
